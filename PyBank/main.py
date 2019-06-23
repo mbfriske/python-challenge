185f752262f8309cf26c17dcf8ca9fe618cc99d7
Matthew Friske <MattF@matthews-mbp.home>
2019-06-22 21:18:29 -0500

    final main.py for pybank and pypoll. adding text files.

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,12 +2,64 @@
 
 import os
 import csv
+import numpy as np
 
-print ("Hi, this is working")
+csvpath = os.path.join("budget_data.csv")
 
-csvpath = os.path.join("..\SMUDataScience⁩\SMUDAL201905DATA5⁩\02-Homework⁩\03-Python⁩\Instructions⁩\PyBank⁩\Resources⁩\budget_data.csv")
+total_month = 0
+pl_total = 0
+average_change_list = []
+dates = []
+
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+
+    first_row = next(csvreader)
+    previous = int(first_row[1])
+    total_month = total_month + 1 
+    pl_total = pl_total + int(first_row[1])
+    dates.append(first_row[0])
+
+    for row in csvreader:
+        total_month = total_month + 1 
+        pl_total = pl_total + int(row[1])
+   
+        average_change = int(row[1]) - previous
+        average_change_list.append(average_change)
+        previous = int(row[1])
+        dates.append(row[0])
+        
+
+average_change_calc = (sum(average_change_list) / (len(average_change_list)))
+
+
+maxIndx = np.argmax(average_change_list) + 1
+minIndx = np.argmin(average_change_list) + 1
+
+maxDate = dates[maxIndx]
+minDate = dates[minIndx]
+
+
+print("Financial Analysis")
+print("---------------------------------------")
+print("Total Months: " + str(total_month))
+print("Total: " + "$" + str(pl_total))
+print(f"Average Change: ${round(average_change_calc, 2)}")
+print("Greatest Increse in Profits: " + str(maxDate) + " ($" + str(max(average_change_list)) + ")")
+print("Greatest Decrease in Profits: " + str(minDate) + " ($" + str(min(average_change_list)) + ")")
+
+fh = open("bankpy_text", "w")
+
+fh.write("Financial Analysis\n")
+fh.write("---------------------------------------\n")
+fh.write("Total Months: " + str(total_month) + "\n")
+fh.write("Total: " + "$" + str(pl_total) + "\n")
+fh.write("Average Change: $" + str(round(average_change_calc, 2)) + "\n")
+fh.write("Greatest Increse in Profits: " + str(maxDate) + " ($" + str(max(average_change_list)) + ")\n")
+fh.write("Greatest Decrease in Profits: " + str(minDate) + " ($" + str(min(average_change_list)) + ")\n")
+
+fh.close()
+
+                
---
+++ PyPoll/main.py
@@ -0,0 +1,93 @@
+# Python Homework - PyPoll (Matt Friske)
+
+import os
+import csv
+import numpy as np
+
+csvpath = os.path.join("election_data.csv")
+
+total = 0
+candidate_list = []
+khan = []
+correy = []
+li = []
+otooley = []
+
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    khan_count = 1
+    correy_count = 1
+    li_count = 1
+    otooley_count = 1
+    total = total + 1
+
+    for row in csvreader:
+        total = total + 1
+        candidate_list.append(row[2])
+
+        if row[2] == "Khan":
+            khan.append(khan_count)
+
+        if row[2] == "Correy":
+            correy.append(correy_count)
+
+        if row[2] == "Li":
+            li.append(li_count)
+
+        if row[2] == "O'Tooley":
+            otooley.append(otooley_count)
+
+
+khan_sum = sum(khan)
+correy_sum = sum(correy)
+li_sum = sum(li)
+otooley_sum = sum(otooley)
+
+winner = "none"
+if khan_sum > correy_sum and khan_sum > li_sum and khan_sum > otooley_sum:
+    winner = "Khan"
+
+if correy_sum > khan_sum and correy_sum > li_sum and correy_sum > otooley_sum:
+    winner = "Correy"
+
+if li_sum > khan_sum and li_sum > correy_sum and li_sum > otooley_sum:
+    winner = "Li"
+
+if otooley_sum > khan_sum and otooley_sum > correy_sum and otooley_sum > li_sum:
+    winner = "O'Tooley"
+
+khan_per = khan_sum / total
+correy_per = correy_sum / total
+li_per = li_sum / total
+otooley_per = otooley_sum / total
+
+
+print("Election Results")
+print("----------------------")
+print("Total Votes: " + str(total))
+print("Khan: " + str("{:.3%}".format(khan_per)) + " (" + str(khan_sum) + ")")
+print("Correy: " + str("{:.3%}".format(correy_per)) + " (" + str(correy_sum) + ")")
+print("Li: " + str("{:.3%}".format(li_per)) + " (" + str(li_sum) + ")")
+print("O'Tooley: " + str("{:.3%}".format(otooley_per)) + " (" + str(otooley_sum) + ")")
+print("----------------------")
+print("Winner: " + str(winner))
+print("----------------------")
+
+
+fh = open("pypoll_text", "w")
+
+fh.write("Election Results\n")
+fh.write("----------------------\n")
+fh.write("Total Votes: " + str(total) + "\n")
+fh.write("Khan: " + str("{:.3%}".format(khan_per)) + " (" + str(khan_sum) + ")\n")
+fh.write("Correy: " + str("{:.3%}".format(correy_per)) + " (" + str(correy_sum) + ")\n")
+fh.write("Li: " + str("{:.3%}".format(li_per)) + " (" + str(li_sum) + ")\n")
+fh.write("O'Tooley: " + str("{:.3%}".format(otooley_per)) + " (" + str(otooley_sum) + ")\n")
+fh.write("----------------------\n")
+fh.write("Winner: " + str(winner) + "\n")
+fh.write("----------------------\n")
+
+fh.close()
